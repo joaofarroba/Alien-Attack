05e01a0043cf73ef27c02060d6ef9d54f5e287e7
João Farroba <148582269+joaofarroba@users.noreply.github.com>
2024-01-08 17:13:34 +0000

    Setting Screensize Position

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -12,4 +12,18 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	#if global_position.x < 0:
+		#global_position.x = 0
+	#if global_position.x > screen_size.x:
+		#global_position.x = screen_size.x
+	#if global_position.y < 0:
+		#global_position.y = 0
+	#if global_position.y > screen_size.y:
+		#global_position.y = screen_size.y
+	
+	#global_position.x = clampf(global_position.x, 0, screen_size.x)
+	#global_position.y = clampf(global_position.y, 0, screen_size.y)
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
 
