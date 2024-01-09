d1fad74794ba36f65a8aa6ceafb9ef49237752ce
João Farroba <148582269+joaofarroba@users.noreply.github.com>
2024-01-09 13:36:37 +0000

    Shooting Rockets and Deleting

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 var speed = 300
+var rscene = preload("res://scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer
+
 func _physics_process(delta):
 	velocity = Vector2(0,0)
 	if Input.is_action_pressed("move_down"):
@@ -14,16 +18,16 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	var screen_size = get_viewport_rect().size
-	#if global_position.x < 0:
-		#global_position.x = 0
-	#if global_position.x > screen_size.x:
-		#global_position.x = screen_size.x
-	#if global_position.y < 0:
-		#global_position.y = 0
-	#if global_position.y > screen_size.y:
-		#global_position.y = screen_size.y
-	
-	#global_position.x = clampf(global_position.x, 0, screen_size.x)
-	#global_position.y = clampf(global_position.y, 0, screen_size.y)
+
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var rocket_instance = rscene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 80
 
--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -1,5 +1,15 @@
 extends Area2D
 
+@export var velocity = 120
+
+@onready var visible_notifier = $VisibleNotifier
+
+func _ready():
+	visible_notifier.connect("screen_exited" , _on_screen_exited)
 
 func _physics_process(delta):
-	global_position.x = global_position.x + 10
+	global_position.x += velocity * delta
+
+func _on_screen_exited():
+	queue_free()
+
